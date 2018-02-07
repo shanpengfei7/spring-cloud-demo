9cd212460d6b03243c434d1bfa790f3f9d8320ba
shanpengfei <shanpengfei7@163.com>
2018-02-07 16:45:22 +0800

    消息总线

--- config-client/src/main/java/com/pnfy/study/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/pnfy/study/configclient/ConfigClientApplication.java
@@ -3,11 +3,13 @@ package com.pnfy.study.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RefreshScope   //配置文件自动刷新，如果不加，server端刷新，client不刷新
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
